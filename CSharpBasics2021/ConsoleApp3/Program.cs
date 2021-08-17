5279b807c0d6613e7a2c37e5a773bed98363a45a
christianhussmann <54892854+christianhussmann@users.noreply.github.com>
2021-08-17 20:48:17 +0200

    from the 40 videoes
    made as homework

---
+++ CSharpBasics2021/ConsoleApp3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = int.Parse(Console.ReadLine());
+
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            
+            bool aNumber = false;
+            var firstInputAsInt = -1;
+           
+            while (!aNumber)
+            {
+                Console.WriteLine("Input the First Number :");
+                var firstInputAsText = Console.ReadLine();
+                try
+                {
+                    // Convert.ToInt32(firstInputAsText);
+
+                    firstInputAsInt = int.Parse(firstInputAsText);
+                    aNumber = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Thats not an int, try again");
+                }
+            }
+
+            Console.WriteLine("Input the Second Number :");
+            var secondInputAsText = Console.ReadLine();
+            int secondInputAsInt;
+
+            while (!int.TryParse(secondInputAsText, out secondInputAsInt))
+            {
+                Console.WriteLine("Thats not an int, try again");
+                Console.WriteLine("Input the Second Number :");
+                secondInputAsText = Console.ReadLine();
+            }
+
+            int temp;
+
+            temp = firstInputAsInt;
+            firstInputAsInt = secondInputAsInt;
+            secondInputAsInt = temp;
+
+            Console.WriteLine("first value: " + firstInputAsInt + " second value: " + secondInputAsInt);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpBasics2021/ConsoleApp6-conditions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp6_conditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
