a2f5556a466456b20eda42081b5ace6ef608c614
christianhussmann <54892854+christianhussmann@users.noreply.github.com>
2021-08-22 22:14:41 +0200

    Add files via upload

--- CSharpBasics2021/ConsoleApp6-conditions/Program.cs
+++ CSharpBasics2021/ConsoleApp6-conditions/Program.cs
@@ -6,7 +6,39 @@ namespace ConsoleApp6_conditions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //AND = &&
+            var a = 10;
+            var b = 0;
+
+            if (a > b && a < 10 && a > 5 )
+            {
+                //Console.WriteLine("A");
+            }
+            else
+            {
+                //Console.WriteLine("B");
+            }
+          
+            //OR = ||
+            var c = 2;
+            var d = 5;
+
+            if ((c > d || c > 1) && a > 5)
+            {
+                Console.WriteLine("C");
+            }
+            else
+            {
+                Console.WriteLine("D");
+            }
+
+            while(c > d || c > 1)
+            {
+                //Console.WriteLine("C");
+                c--;
+            }
+
+            Console.ReadLine();
         }
     }
 }
